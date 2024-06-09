b3ef0602ba1a6a39e97e88c3ebfb9c5aebea54fb
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-06-09 12:25:10 +1000

    day3a working but not well understood

--- src/day3.rs
+++ src/day3.rs
@@ -2,83 +2,75 @@ use std::{collections::VecDeque, thread::current};
 
 use crate::filereading;
 
-fn check_alone(line_bytes: &Option<&[u8]>, number_start: usize, number_length: usize) -> bool {
-    if line_bytes.is_none() {
-        return true;
+fn found_other(lines: &[String], index: i32, start: usize, end: usize) -> bool {
+    if index < 0 || index > lines.len() as i32 - 1 {
+        return false;
     }
 
-    let line_bytes = line_bytes.unwrap();
-
-    let start = if number_start == 0 {
-        0
-    } else {
-        number_start - 1
-    };
-    let number_end = number_start + number_length;
-    let end = if number_end >= line_bytes.len() - 1 {
-        number_end
-    } else {
-        number_end + 1
-    };
-    let search_bytes = &line_bytes[start..end];
+    let safe_index = index as usize;
+    let search_bytes = &lines[safe_index].as_bytes()[start..end];
     for byte_ref in search_bytes {
         let search_char = *byte_ref as char;
         if !search_char.is_numeric() && search_char != '.' {
-            return false;
+            return true;
         }
     }
-    true
+    false
 }
 
 fn a() {
     let lines = filereading::get_lines("src/inputs/day3.txt")
         .map_while(Result::ok)
         .collect::<Vec<String>>();
+    let mut sum = 0;
 
     for (line_index, line) in lines.iter().enumerate() {
-        let previous_bytes = if line_index == 0 {
-            None
-        } else {
-            lines.get(line_index - 1).map(|l| l.as_bytes())
-        };
-        let next_bytes = lines.get(line_index + 1).map(|l| l.as_bytes());
-
         let mut char_index = 0;
         let bytes = line.as_bytes();
         while char_index < line.len() {
             let current_char = bytes[char_index] as char;
-            let parse_check = current_char.to_string().parse::<usize>();
 
-            if parse_check.is_err() {
+            if !current_char.is_numeric() {
                 char_index += 1;
                 continue;
             }
 
-            let mut number_length = 1;
-            let mut value = parse_check.unwrap();
+            let mut number_string: Vec<char> = vec![current_char];
             loop {
-                let check_index = char_index + number_length;
+                let check_index = char_index + number_string.len();
                 if check_index > line.len() - 1 {
                     break;
                 }
-                let parse_res = (bytes[check_index] as char).to_string().parse::<usize>();
-                match parse_res {
-                    Ok(next_val) => {
-                        value = 10 * value + next_val;
-                        number_length += 1
-                    }
-                    Err(_) => break,
+                let next_char = bytes[check_index] as char;
+                if !next_char.is_numeric() {
+                    break;
                 }
+                number_string.push(next_char)
             }
 
-            let previous_alone = check_alone(&previous_bytes, char_index, number_length);
-            let next_alone = check_alone(&next_bytes, char_index, number_length);
-            let mut alone = previous_alone || next_alone;
-            println!("{value} {alone}");
+            let start = if char_index > 0 { char_index - 1 } else { 0 };
+            let possible_end = char_index + number_string.len() + 1;
+            let end = if possible_end >= line.len() {
+                line.len() - 1
+            } else {
+                possible_end
+            };
 
-            char_index += number_length
+            let previous_other = found_other(&lines, line_index as i32 - 1, start, end);
+            let current_other = found_other(&lines, line_index as i32, start, end);
+            let next_other = found_other(&lines, line_index as i32 + 1, start, end);
+            let other = previous_other || current_other || next_other;
+
+            if other {
+                let collected_string = number_string.iter().collect::<String>();
+                let number: i32 = collected_string.parse().expect("Number should be parsable");
+                sum += number;
+            }
+
+            char_index += number_string.len()
         }
     }
+    println!("{sum}")
 }
 
 pub fn run() {
