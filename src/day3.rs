3723a18fa18837d510952f7e3299c73102ba5072
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-06-09 22:42:47 +1000

    day3b

--- src/day3.rs
+++ src/day3.rs
@@ -1,5 +1,3 @@
-use std::{collections::VecDeque, thread::current};
-
 use crate::filereading;
 
 fn found_other(lines: &[String], index: i32, start: usize, end: usize) -> bool {
@@ -73,6 +71,102 @@ fn a() {
     println!("{sum}")
 }
 
+fn search_step(chars: &[char], start: usize, step: i32) -> usize {
+    let mut index = start as i32 + step;
+
+    loop {
+        let char = match chars.get(index as usize) {
+            None => break,
+            Some(res) => res,
+        };
+
+        if !char.is_numeric() {
+            break;
+        }
+
+        index += step;
+    }
+
+    (index - step) as usize
+}
+
+fn search_nearby_number(line: Option<&String>, index: usize, matched_numbers: &mut Vec<i32>) {
+    if line.is_none() {
+        return;
+    }
+
+    let chars = line
+        .unwrap()
+        .as_bytes()
+        .iter()
+        .map(|&b| b as char)
+        .collect::<Vec<char>>();
+
+    let start_index = if index == 0 { 0 } else { index - 1 };
+    let end_index = if index == chars.len() - 1 {
+        chars.len() - 1
+    } else {
+        index + 1
+    };
+
+    let mut check_index = start_index;
+
+    while check_index < end_index + 1 {
+        let check_char = match chars.get(check_index) {
+            None => break,
+            Some(res) => res,
+        };
+
+        if !check_char.is_numeric() {
+            check_index += 1;
+            continue;
+        }
+
+        let search_end_index = search_step(&chars, check_index, 1);
+        let search_start_index = search_step(&chars, check_index, -1);
+
+        let number = &chars[search_start_index..search_end_index + 1]
+            .iter()
+            .collect::<String>()
+            .parse::<i32>()
+            .expect("Parsable");
+
+        matched_numbers.push(*number);
+
+        check_index = search_end_index + 1;
+    }
+}
+
+fn b() {
+    let lines = filereading::get_lines("src/inputs/day3.txt")
+        .map_while(Result::ok)
+        .collect::<Vec<String>>();
+    let mut sum = 0;
+
+    for (line_index, line) in lines.iter().enumerate() {
+        let bytes = line.as_bytes();
+
+        for (char_index, byte) in bytes.iter().enumerate() {
+            let character = *byte as char;
+
+            if character != '*' {
+                continue;
+            }
+
+            let mut matched_numbers: Vec<i32> = vec![];
+
+            search_nearby_number(lines.get(line_index - 1), char_index, &mut matched_numbers);
+            search_nearby_number(Some(line), char_index, &mut matched_numbers);
+            search_nearby_number(lines.get(line_index + 1), char_index, &mut matched_numbers);
+
+            if matched_numbers.len() == 2 {
+                sum += matched_numbers[0] * matched_numbers[1];
+            }
+        }
+    }
+    println!("{sum}")
+}
+
 pub fn run() {
-    a();
+    b();
 }
