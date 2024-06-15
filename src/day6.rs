e07e423556770f2e29c54a9a36736d29b2abf824
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-06-15 19:59:35 +1000

    day6b done

--- src/day6.rs
+++ src/day6.rs
@@ -1,12 +1,12 @@
 use crate::filereading;
 
 pub fn run() {
-    a();
+    b();
 }
 
 struct Race {
-    time: i32,
-    distance: i32,
+    time: i64,
+    distance: i64,
 }
 
 fn extract_numbers(line: String) -> Vec<i32> {
@@ -21,18 +21,23 @@ fn extract_numbers(line: String) -> Vec<i32> {
     numbers
 }
 
-fn get_races() -> Vec<Race> {
+fn get_times_distances() -> (Vec<i32>, Vec<i32>) {
     let mut lines = filereading::get_lines("src/inputs/day6.txt");
 
     let times = extract_numbers(lines.next().unwrap().unwrap());
     let distances = extract_numbers(lines.next().unwrap().unwrap());
 
+    (times, distances)
+}
+
+fn get_races() -> Vec<Race> {
+    let (times, distances) = get_times_distances();
     times
         .into_iter()
         .enumerate()
         .map(|(i, time)| Race {
-            time,
-            distance: distances[i],
+            time: time.into(),
+            distance: distances[i].into(),
         })
         .collect::<Vec<Race>>()
 }
@@ -60,4 +65,38 @@ fn a() {
     }
 
     println!("{product}")
+}
+
+fn aggregate_numbers(numbers: Vec<i32>) -> i64 {
+    let mut final_string = String::from("");
+
+    for (i, number) in numbers.iter().enumerate() {
+        final_string.push_str(&number.to_string())
+    }
+
+    final_string.parse::<i64>().unwrap()
+}
+
+fn b() {
+    let (times, distances) = get_times_distances();
+
+    let total_time = aggregate_numbers(times);
+    let total_distance = aggregate_numbers(distances);
+
+    println!("{total_time}, {total_distance}");
+
+    let mut would_win = 0;
+
+    for charge_time in 1..total_time {
+        let running_time = total_time - charge_time;
+        let speed = charge_time;
+
+        let distance = running_time * speed;
+
+        if distance > total_distance {
+            would_win += 1;
+        }
+    }
+
+    println!("{would_win}")
 }
