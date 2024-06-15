f4b90cdfa8447771d2a04f4efa42ec640f08d554
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-06-15 19:03:23 +1000

    am i too good at programming or what

--- src/day5.rs
+++ src/day5.rs
@@ -1,4 +1,8 @@
-use std::{cmp::max, i64::MAX, str::FromStr};
+use std::{
+    cmp::{max, min},
+    i64::MAX,
+    str::FromStr,
+};
 
 use crate::filereading;
 
@@ -45,52 +49,63 @@ impl Map {
     }
 
     fn get_overlap(&self, range: &Range) -> Option<(Range, Vec<Range>)> {
-        let range_end_index = range.start + range.length - 1;
+        let map_start_index = self.source_start;
         let map_end_index = self.source_start + self.range_length - 1;
 
-        let overlap_start_index = max(range.start, self.source_start);
-        let overlap_end_index = std::cmp::min(range_end_index, map_end_index);
+        let range_start_index = range.start;
+        let range_end_index = range.start + range.length - 1;
 
-        println!("{:?} {:?}", range, self);
-        println!("{overlap_start_index} {overlap_end_index}");
+        println!("{map_start_index} {map_end_index} {range_start_index} {range_end_index}");
 
-        let overlap_length = overlap_end_index - overlap_start_index + 1;
+        let overlap_start_index = max(map_start_index, range_start_index);
+        let overlap_end_index = min(map_end_index, range_end_index);
 
-        if overlap_length < 0 {
+        if overlap_end_index < overlap_start_index {
             return None;
         }
+
+        let range_prior_start_index = min(overlap_start_index, range_start_index);
+        let range_prior_end_index = overlap_start_index - 1;
+
+        let range_posterior_start_index = overlap_end_index + 1;
+        let range_posterior_end_index = max(overlap_end_index, range_end_index);
+
         let offset = self.destination_start - self.source_start;
-        let changed_range = Range {
-            start: overlap_start_index + offset,
-            length: overlap_length,
+        let new_start = overlap_start_index + offset;
+
+        println!("Mapped!: {overlap_start_index} -> {new_start}");
+        let overlap_range = Range {
+            start: new_start,
+            length: overlap_end_index - overlap_start_index + 1,
         };
 
-        let mut others: Vec<Range> = vec![];
+        let mut ignored = vec![];
 
-        let length_missed_before = overlap_start_index - range.start;
+        let prior_ignored_length = range_prior_end_index - range_prior_start_index + 1;
+        let posterior_ignored_length = range_posterior_end_index - range_posterior_start_index + 1;
 
-        if length_missed_before > 0 {
-            others.push(Range {
-                start: range.start,
-                length: length_missed_before,
+        if prior_ignored_length > 0 {
+            ignored.push(Range {
+                start: range_prior_start_index,
+                length: prior_ignored_length,
             })
         }
 
-        let length_missed_after = range_end_index - overlap_end_index;
-
-        if length_missed_after > 0 {
-            others.push(Range {
-                start: overlap_end_index + 1,
-                length: length_missed_after,
+        if posterior_ignored_length > 0 {
+            ignored.push(Range {
+                start: range_posterior_start_index,
+                length: posterior_ignored_length,
             })
         }
 
-        Some((changed_range, others))
+        println!("{:?} {:?}", overlap_range, ignored);
+
+        Some((overlap_range, ignored))
     }
 }
 
 fn get_map_groups() -> (Vec<i64>, Vec<Vec<Map>>) {
-    let mut lines = filereading::get_lines("src/inputs/day5e.txt");
+    let mut lines = filereading::get_lines("src/inputs/day5.txt");
 
     let first_line = lines.next().unwrap().unwrap();
 
@@ -163,42 +178,48 @@ fn b() {
     let mut seed_ranges: Vec<Range> = vec![];
     for i in 0..(seeds.len() / 2) {
         seed_ranges.push(Range {
-            start: seeds[i],
-            length: seeds[i + 1],
+            start: seeds[2 * i],
+            length: seeds[2 * i + 1],
         })
     }
 
-    let mut current_ranges: Vec<Range> = seed_ranges;
+    let mut current_ranges = seed_ranges;
     let mut next_ranges: Vec<Range> = vec![];
     for map_group in map_groups {
-        for &range in current_ranges.iter() {
-            let mut ranges_to_map = vec![range];
+        for range in current_ranges.iter() {
+            let mut check_sections: Vec<Range> = vec![range.clone()];
 
-            while ranges_to_map.len() > 0 {
-                let range_to_map = ranges_to_map[0];
-
-                let mut mapped_range: Option<Range> = None;
-                for map in map_group.iter() {
-                    match map.get_overlap(&range_to_map) {
-                        None => {}
-                        Some((res_range, uncaptured)) => {
-                            ranges_to_map.extend(uncaptured);
-                            mapped_range = Some(res_range);
-                            break;
-                        }
+            while let Some(range_to_check) = check_sections.pop() {
+                let mut utilised: bool = false;
+                for group in map_group.iter() {
+                    if let Some((new_range, uncovered)) = group.get_overlap(&range_to_check) {
+                        check_sections.extend(uncovered);
+                        next_ranges.push(new_range);
+                        utilised = true;
+                        break;
                     }
                 }
 
-                match mapped_range {
-                    None => next_ranges.push(range_to_map.clone()),
-                    Some(res) => next_ranges.push(res),
+                if !utilised {
+                    next_ranges.push(range_to_check)
                 }
             }
         }
 
+        // let mut total_sum = 0;
+        // for range in next_ranges.iter() {
+        //     total_sum += range.length;
+        // }
+        // println!("Length: {:?}", total_sum);
         current_ranges.clone_from(&next_ranges);
-        next_ranges.clear();
+        next_ranges.clear()
     }
+
+    let mut minimum = i64::MAX;
+    for range in current_ranges {
+        minimum = min(range.start, minimum)
+    }
+    println!("{minimum}")
 }
 
 pub fn run() {
