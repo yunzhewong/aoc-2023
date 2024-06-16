2e533b31d295b7c6ec3e2c4c9d79d4b8c1d1e23c
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-06-16 19:23:16 +1000

    day7b

--- src/day7.rs
+++ src/day7.rs
@@ -1,22 +1,158 @@
-use std::{cmp::Ordering, str::FromStr, thread::current};
+use std::{cmp::Ordering, str::FromStr};
 
 use crate::filereading;
 
 pub fn run() {
-    a();
-}
-
-#[derive(Debug)]
-struct ParseError;
-
-#[derive(Debug, Eq, Clone)]
-struct Card {
-    label: usize,
+    b();
 }
 
 const CARD_OPTIONS: [char; 13] = [
     'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2',
 ];
+
+const CARD_OPTIONS_WITH_JOKER: [char; 13] = [
+    'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J',
+];
+
+fn create_card(label: char, options: &[char]) -> Card {
+    for (i, option) in options.iter().enumerate() {
+        if *option == label {
+            return Card { label: i };
+        }
+    }
+
+    panic!();
+}
+
+fn identify_multiples(cards: Vec<Card>) -> Vec<Multiples> {
+    let mut multiples: Vec<Multiples> = vec![];
+    let mut current_multiples: Option<Multiples> = None;
+    for card in cards.iter() {
+        match current_multiples {
+            None => {
+                current_multiples = Some(Multiples {
+                    card: card.clone(),
+                    count: 1,
+                })
+            }
+            Some(ref mut val) => {
+                if val.card == *card {
+                    *val = Multiples {
+                        count: val.count + 1,
+                        card: val.card.clone(),
+                    };
+                } else {
+                    multiples.push(Multiples {
+                        count: val.count,
+                        card: val.card.clone(),
+                    });
+                    *val = Multiples {
+                        card: card.clone(),
+                        count: 1,
+                    }
+                }
+            }
+        }
+    }
+
+    if let Some(val) = current_multiples {
+        multiples.push(Multiples {
+            count: val.count,
+            card: val.card,
+        });
+    }
+
+    multiples
+}
+
+fn get_hand_type(multiples: Vec<Multiples>) -> HandType {
+    match &multiples[..] {
+        [_] => HandType::FiveOfAKind,
+        [first, _] => match first.count {
+            4 => HandType::FourOfAKind,
+            3 => HandType::FullHouse,
+            _ => HandType::HighCard,
+        },
+        [first, _, _] => match first.count {
+            3 => HandType::ThreeOfAKind,
+            2 => HandType::TwoPair,
+            _ => HandType::HighCard,
+        },
+        [_, _, _, _] => HandType::OnePair,
+        _ => HandType::HighCard,
+    }
+}
+
+fn group_cards(mut cards: Vec<Card>) -> (Vec<Card>, Vec<Multiples>) {
+    let original_copy = cards.clone();
+    cards.sort();
+
+    let mut multiples = identify_multiples(cards);
+    multiples.sort_by(|a, b| b.count.cmp(&a.count));
+
+    (original_copy, multiples)
+}
+
+fn parse_cards(s: &str, options: &[char]) -> Vec<Card> {
+    s.chars()
+        .map(|c| create_card(c, options))
+        .collect::<Vec<Card>>()
+}
+
+fn create_basic_hand(s: &str) -> Hand {
+    let cards = parse_cards(s, &CARD_OPTIONS);
+    let (original_copy, multiples) = group_cards(cards);
+    let hand_type = get_hand_type(multiples);
+
+    Hand {
+        hand_type,
+        cards: original_copy,
+    }
+}
+
+fn create_joker_hand(s: &str) -> Hand {
+    let cards = parse_cards(s, &CARD_OPTIONS_WITH_JOKER);
+    let (original_copy, mut multiples) = group_cards(cards);
+
+    let mut joker_index = None;
+    for (i, multiple) in multiples.iter().enumerate() {
+        if multiple.card.label == CARD_OPTIONS_WITH_JOKER.len() - 1 {
+            joker_index = Some(i);
+        }
+    }
+
+    if let Some(i) = joker_index {
+        if multiples.len() != 1 {
+            let joker_multiple = multiples.remove(i);
+
+            multiples[0].count += joker_multiple.count
+        }
+    }
+
+    let hand_type = get_hand_type(multiples);
+
+    Hand {
+        hand_type,
+        cards: original_copy,
+    }
+}
+
+fn identify_round_sections(s: &str) -> (&str, i32) {
+    let sections = s.split(' ').collect::<Vec<&str>>();
+
+    let hand_section = sections[0];
+    let bid_section = sections[1].parse::<i32>().unwrap();
+
+    (hand_section, bid_section)
+}
+
+#[derive(Debug)]
+struct ParseError;
+
+#[derive(Debug, Eq, Clone, PartialEq)]
+struct Card {
+    label: usize,
+}
 
 impl Ord for Card {
     fn cmp(&self, other: &Self) -> Ordering {
@@ -27,24 +163,6 @@ impl Ord for Card {
 impl PartialOrd for Card {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
-    }
-}
-
-impl PartialEq for Card {
-    fn eq(&self, other: &Self) -> bool {
-        self.label == other.label
-    }
-}
-
-impl Card {
-    fn from_char(c: char) -> Card {
-        for (i, option) in CARD_OPTIONS.iter().enumerate() {
-            if *option == c {
-                return Card { label: i };
-            }
-        }
-
-        panic!();
     }
 }
 
@@ -94,76 +212,6 @@ struct Multiples {
     count: i32,
 }
 
-impl FromStr for Hand {
-    type Err = ParseError;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut cards = s.chars().map(Card::from_char).collect::<Vec<Card>>();
-
-        let original_copy = cards.clone();
-        cards.sort();
-
-        let mut multiples: Vec<Multiples> = vec![];
-        let mut current_multiples: Option<Multiples> = None;
-        for card in cards.iter() {
-            match current_multiples {
-                None => {
-                    current_multiples = Some(Multiples {
-                        card: card.clone(),
-                        count: 1,
-                    })
-                }
-                Some(ref mut val) => {
-                    if val.card == *card {
-                        *val = Multiples {
-                            count: val.count + 1,
-                            card: val.card.clone(),
-                        };
-                    } else {
-                        multiples.push(Multiples {
-                            count: val.count,
-                            card: val.card.clone(),
-                        });
-                        *val = Multiples {
-                            card: card.clone(),
-                            count: 1,
-                        }
-                    }
-                }
-            }
-        }
-
-        if let Some(val) = current_multiples {
-            multiples.push(Multiples {
-                count: val.count,
-                card: val.card,
-            });
-        }
-
-        multiples.sort_by(|a, b| b.count.cmp(&a.count));
-
-        let handType = match &multiples[..] {
-            [_] => HandType::FiveOfAKind,
-            [first, _] => match first.count {
-                4 => HandType::FourOfAKind,
-                3 => HandType::FullHouse,
-                _ => HandType::HighCard,
-            },
-            [first, _, _] => match first.count {
-                3 => HandType::ThreeOfAKind,
-                2 => HandType::TwoPair,
-                _ => HandType::HighCard,
-            },
-            [_, _, _, _] => HandType::OnePair,
-            _ => HandType::HighCard,
-        };
-
-        Ok(Hand {
-            hand_type: handType,
-            cards: original_copy,
-        })
-    }
-}
-
 #[derive(Debug, Eq)]
 struct Round {
     hand: Hand,
@@ -194,29 +242,59 @@ impl FromStr for Round {
         let sections = s.split(' ').collect::<Vec<&str>>();
 
         Ok(Round {
-            hand: Hand::from_str(sections[0]).unwrap(),
+            hand: create_basic_hand(sections[0]),
             bid: sections[1].parse::<i32>().unwrap(),
         })
     }
 }
 
-fn a() {
-    let lines = filereading::get_lines("src/inputs/day7.txt");
-
-    let mut rounds: Vec<Round> = lines
-        .map_while(Result::ok)
-        .map(|l| Round::from_str(&l).unwrap())
-        .collect();
-
+fn count_rounds(mut rounds: Vec<Round>) {
     rounds.sort();
     rounds.reverse();
+
+    for round in rounds.iter() {
+        println!("{:?}", round);
+    }
 
     let mut total = 0;
     for (rank, round) in rounds.iter().enumerate() {
         let good_rank: i32 = rank.try_into().unwrap();
-        println!("{rank} {:?}", round);
         total += (good_rank + 1) * round.bid
     }
 
     println!("{:?}", total)
+}
+
+fn a() {
+    let lines = filereading::get_lines("src/inputs/day7.txt");
+
+    let rounds: Vec<Round> = lines
+        .map_while(Result::ok)
+        .map(|l| {
+            let (hand_section, bid) = identify_round_sections(&l);
+            Round {
+                hand: create_basic_hand(hand_section),
+                bid,
+            }
+        })
+        .collect();
+
+    count_rounds(rounds)
+}
+
+fn b() {
+    let lines = filereading::get_lines("src/inputs/day7.txt");
+
+    let rounds: Vec<Round> = lines
+        .map_while(Result::ok)
+        .map(|l| {
+            let (hand_section, bid) = identify_round_sections(&l);
+            Round {
+                hand: create_joker_hand(hand_section),
+                bid,
+            }
+        })
+        .collect();
+
+    count_rounds(rounds)
 }
