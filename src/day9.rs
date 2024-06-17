fa4cfaac6c770d2987f8916a51380ff5a41fc3f0
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-06-17 19:29:21 +1000

    day9a

---
+++ src/day9.rs
@@ -0,0 +1,54 @@
+use crate::filereading;
+
+pub fn run() {
+    a()
+}
+
+fn complete(row: &Vec<i32>) -> bool {
+    for number in row {
+        if *number != 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn a() {
+    let lines = filereading::get_lines("src/inputs/day9.txt");
+
+    let mut total = 0;
+    for line in lines.map_while(Result::ok) {
+        let numbers = line
+            .split(' ')
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+
+        let mut rows = vec![numbers];
+
+        while !complete(&rows[rows.len() - 1]) {
+            let mut next_row: Vec<i32> = vec![];
+            let current_row = &rows[rows.len() - 1];
+
+            for i in 0..current_row.len() - 1 {
+                let difference = current_row[i + 1] - current_row[i];
+                next_row.push(difference);
+            }
+
+            rows.push(next_row);
+        }
+
+        rows.reverse();
+
+        let mut iterable_rows = rows.into_iter();
+
+        let mut last_difference = 0;
+        iterable_rows.next();
+        for row in iterable_rows {
+            let final_value = row[row.len() - 1];
+            last_difference += final_value;
+        }
+
+        total += last_difference
+    }
+    println!("{total}")
+}
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ mod day5;
 mod day6;
 mod day7;
 mod day8;
+mod day9;
 mod filereading;
 
 fn main() {
     // day1::run()
-    day8::run()
+    day9::run()
 }
