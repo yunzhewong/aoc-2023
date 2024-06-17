200f717dd81ebcd5be06f11c321f9b57c1083287
yunzhewong <84904965+yunzhewong@users.noreply.github.com>
2024-06-17 19:36:12 +1000

    day9b

--- src/day9.rs
+++ src/day9.rs
@@ -1,7 +1,7 @@
 use crate::filereading;
 
 pub fn run() {
-    a()
+    b()
 }
 
 fn complete(row: &Vec<i32>) -> bool {
@@ -13,29 +13,35 @@ fn complete(row: &Vec<i32>) -> bool {
     true
 }
 
+fn define_history(line: String) -> Vec<Vec<i32>> {
+    let numbers = line
+        .split(' ')
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    let mut rows = vec![numbers];
+
+    while !complete(&rows[rows.len() - 1]) {
+        let mut next_row: Vec<i32> = vec![];
+        let current_row = &rows[rows.len() - 1];
+
+        for i in 0..current_row.len() - 1 {
+            let difference = current_row[i + 1] - current_row[i];
+            next_row.push(difference);
+        }
+
+        rows.push(next_row);
+    }
+
+    rows
+}
+
 fn a() {
     let lines = filereading::get_lines("src/inputs/day9.txt");
 
     let mut total = 0;
     for line in lines.map_while(Result::ok) {
-        let numbers = line
-            .split(' ')
-            .map(|s| s.parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();
-
-        let mut rows = vec![numbers];
-
-        while !complete(&rows[rows.len() - 1]) {
-            let mut next_row: Vec<i32> = vec![];
-            let current_row = &rows[rows.len() - 1];
-
-            for i in 0..current_row.len() - 1 {
-                let difference = current_row[i + 1] - current_row[i];
-                next_row.push(difference);
-            }
-
-            rows.push(next_row);
-        }
+        let mut rows = define_history(line);
 
         rows.reverse();
 
@@ -49,6 +55,29 @@ fn a() {
         }
 
         total += last_difference
+    }
+    println!("{total}")
+}
+
+fn b() {
+    let lines = filereading::get_lines("src/inputs/day9.txt");
+
+    let mut total = 0;
+    for line in lines.map_while(Result::ok) {
+        let mut rows = define_history(line);
+
+        rows.reverse();
+
+        let mut iterable_rows = rows.into_iter();
+
+        let mut first_difference = 0;
+        iterable_rows.next();
+        for row in iterable_rows {
+            let final_value = row.first().unwrap();
+            first_difference = final_value - first_difference;
+        }
+
+        total += first_difference
     }
     println!("{total}")
 }
